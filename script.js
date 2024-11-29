372bacee05fa06b11c801fb15a7e615630de57ac
Padma-darsi <125295009+Padma-darsi@users.noreply.github.com>
2024-11-29 13:32:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function makeid(l) {
-  // write your code here
+  // Define the characters to include in the random string
+  const characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const charactersLength = characters.length;
+  let result = "";
+
+  // Generate the random string of length l
+  for (let i = 0; i < l; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
 }
 
-// Do not change the code below.
+// Do not change the code below
 const l = prompt("Enter a number.");
-alert(makeid(l));
+alert(makeid(parseInt(l, 10)));
